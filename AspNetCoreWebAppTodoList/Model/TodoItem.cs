736f238e4fc8568d6898c420e3d41724fea63a79
David Störmer <badf00d@posteo.de>
2018-11-30 13:25:57 +0100

    Moved Swagger documentation from /swagger/v1/swagger.json to /api-docs/v1/swagger.json

--- AspNetCoreWebAppTodoList/Model/TodoItem.cs
+++ AspNetCoreWebAppTodoList/Model/TodoItem.cs
@@ -2,8 +2,17 @@
 {
     public class TodoItem
     {
+        /// <summary>
+        /// Internal id
+        /// </summary>
         public long Id { get; set; }
+        /// <summary>
+        /// Title of ToDo-Item
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Defines if todo is done or not.
+        /// </summary>
         public bool IsComplete { get; set; }
     }
 }
--- AspNetCoreWebAppTodoList/Startup.cs
+++ AspNetCoreWebAppTodoList/Startup.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace AspNetCoreWebAppTodoList
@@ -30,8 +21,8 @@ namespace AspNetCoreWebAppTodoList
         public void Configure(IApplicationBuilder appBuilder)
         {
             appBuilder.UseMvc();
-            appBuilder.UseSwagger();
-            appBuilder.UseSwaggerUI(o => o.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1"));
+            appBuilder.UseSwagger(o => o.RouteTemplate = "/api-docs/{documentName}/swagger.json");
+            appBuilder.UseSwaggerUI(o => o.SwaggerEndpoint("/api-docs/v1/swagger.json", "My API v1"));
         }
     }
 }
