1436e8503c71fde801dd75dacb2d7bc380af7f51
David Störmer <badf00d@posteo.de>
2018-11-30 14:36:02 +0100

    Added documentation for different response codes.

--- AspNetCoreWebAppTodoList/Controllers/TodoController.cs
+++ AspNetCoreWebAppTodoList/Controllers/TodoController.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreWebAppTodoList.Controllers
 {
+    /// <summary>
+    /// Controller for manipulation ToDo-Items.
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class TodoController : ControllerBase
@@ -41,6 +44,8 @@ namespace AspNetCoreWebAppTodoList.Controllers
         /// Gets single ToDo-Item by its Id.
         /// </summary>
         /// <param name="id">It from ToDo-Items to get.</param>
+        /// <response code="200">Returns ToDo-Item.</response>
+        /// <response code="404">Returns ToDo-Item with given Id not found.</response>
         /// <returns></returns>
         [HttpGet("{id}", Name = "GetTodo")]
         [ProducesResponseType(typeof(TodoItem), 200)]
@@ -55,6 +60,8 @@ namespace AspNetCoreWebAppTodoList.Controllers
         /// Creates a ToDo-Item.
         /// </summary>
         /// <param name="item">ToDo-Items to create.</param>
+        /// <response code="201">ToDo-Item created successfully.</response>
+        /// <response code="400">Invalid or missing properties.</response>
         /// <returns></returns>
         [HttpPost]
         [ProducesResponseType(201)]
@@ -71,6 +78,9 @@ namespace AspNetCoreWebAppTodoList.Controllers
         /// </summary>
         /// <param name="id">Id of ToDo-Item to update.</param>
         /// <param name="item">Data of ToDo-Item.</param>
+        /// <response code="204">ToDo-Items updated successfully.</response>
+        /// <response code="400">Invalid or missing properties.</response>
+        /// <response code="404">ToDo-Items with given Id not found.</response>
         /// <returns></returns>
         [HttpPut("{id}")]
         [ProducesResponseType(204)]
@@ -92,6 +102,8 @@ namespace AspNetCoreWebAppTodoList.Controllers
         /// Delete a ToDo-Item.
         /// </summary>
         /// <param name="id">Id of ToDo-Item to delete.</param>
+        /// <response code="204">ToDo-Items deleted successfully.</response>
+        /// <response code="404">ToDo-Items with given Id not found.</response>
         /// <returns></returns>
         [HttpDelete("{id}")]
         [ProducesResponseType(204)]
