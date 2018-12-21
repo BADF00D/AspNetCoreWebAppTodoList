9cca0564d267cba60380ad59aee920c963abb3d7
David Störmer <badf00d@posteo.de>
2018-12-21 14:59:58 +0100

    Running with Topshelf

---
+++ AspNetCoreWebAppTodoList/Service.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetCoreWebAppTodoList
+{
+    internal class Service : IDisposable
+    {
+        private IWebHost _webHost;
+
+        public void Dispose()
+        {
+            _webHost?.Dispose();
+        }
+
+        public void Start()
+        {
+            _webHost = new WebHostBuilder()
+                .UseKestrel(options => options.ListenLocalhost(9999))
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+            _webHost.Start();
+        }
+
+        public void Stop()
+        {
+            _webHost.Dispose();
+        }
+    }
+}
